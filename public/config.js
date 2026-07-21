81536360e004aa16aee50f8f6bc959953c183fdd
Pirate13Thebes <gnagnegaelephraim2@gmail.com>
2026-07-21 11:58:51 +0400

    RT-KNITS admin cameras

--- public/config.js
+++ public/config.js
@@ -5,7 +5,7 @@
 window.NITA_CONFIG = {
   USE_REAL_SUPABASE: true,
   SUPABASE_URL: "https://zemhcqrnlfikvtgknqjp.supabase.co",
-  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InplbWhjcXJubGZpa3Z0Z2tucWpwIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc4MzUwNTA1OCwiZXhwIjoyMDk5MDgxMDU4fQ.F90PiAoiZRsWfuGSRfJA5p1hyiorZg8dGpPpN3h58Sw",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InplbWhjcXJubGZpa3Z0Z2tucWpwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM1MDUwNTgsImV4cCI6MjA5OTA4MTA1OH0.lRoJrxLaHehKetF4vaTkc8n67pVHbVRm38zBCikquvk",
   NITA_API_URL: "https://bot.nelsonfodjo.me/webhook",
   NITA_WHATSAPP: "+15551564344"
 };
